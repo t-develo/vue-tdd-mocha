9bc40c47bde9e0c89bd5b83b30682968eb6d11eb
t-develo <kamitika93@gmail.com>
2021-08-04 09:52:40 +0900

    テストインターフェースをtdd仕様に変更

--- tests/unit/Counter.spec.js
+++ tests/unit/Counter.spec.js
@@ -2,12 +2,12 @@ import { shallowMount, mount } from '@vue/test-utils';
 import { assert } from 'chai';
 import Counter from '../../src/components/Counter.vue';
 
-describe('Counter.vue', () => {
-    it('increments count when button before click', () => {
+suite('Counter.vue', () => {
+    test('increments count when button before click', () => {
         const wrapper = shallowMount(Counter);
         assert.include(wrapper.find('div').text(), '0');
     });
-    it('increments count when button is clicked', async () => {
+    test('increments count when button is clicked', async () => {
         const wrapper = mount(Counter);
         const button = wrapper.find('button');
         await button.trigger('click');
---
+++ tests/unit/Home.spec.js
@@ -0,0 +1,10 @@
+﻿import { shallowMount } from '@vue/test-utils';
+import { assert } from 'chai';
+import Home from '../../src/views/Home.vue';
+
+suite('Home.vue', () => {
+    test('Mount Instance', () => {
+        const wrapper = shallowMount(Home);
+        assert.isNotNull(wrapper.vm);
+    });
+});
